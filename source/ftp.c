4add49dc2f72ce99657e20c7fb546f190661399e
Raph <ran.demoulin@gmail.com>
2023-03-29 14:27:13 +0200

    ajout repertoire

--- source/ftp.c
+++ source/ftp.c
@@ -14,45 +14,27 @@ void ftp(int connfd) {
     off_t buf_off;
     char *buf, path[MAXLINE];
     strcpy(path,"./serveur_file/");
-    // Intialise la lecture du socket
-    // Rio_readinitb(&rio, connfd);
 
     // Recupère la taille du nom de fichier
     Rio_readn(connfd, &t_nomf, sizeof(size_t));
-    fprintf(stderr, "%ld\n", t_nomf);
-    // Recupère le contenue du socket
+    // Alloue et recupère le contenue du socket
     buf = Calloc(t_nomf,sizeof(char)*t_nomf);
     Rio_readn(connfd, buf, sizeof(char)*t_nomf);
-    fprintf(stderr, "buf : %ld\n", strlen(buf));    
-    fprintf(stderr, "%s\n", buf);
-    
-    fprintf(stderr, "aaaaaaaaaaaaaaaaaaa\n");
-    fprintf(stderr, "%s\n", buf);
-    fprintf(stderr, "bbbbbbbbbbbbbbbbbbb\n");
 
     while ((f = open(buf, O_RDONLY, 0)) < 0){ /* le fichier n'existe pas */
         code_sortie = 1;
         Rio_writen(connfd, &code_sortie, sizeof(int));
-        buf = NULL;
         Free(buf);
 
         // Recupère la taille du nom de fichier
         Rio_readn(connfd, &t_nomf, sizeof(size_t));
-        fprintf(stderr, "%ld\n", t_nomf);
         // Recupère le contenue du socket
         buf = Calloc(t_nomf,sizeof(char)*t_nomf);
         Rio_readn(connfd, buf, sizeof(char)*t_nomf);
-        fprintf(stderr, "%ld\n", strlen(buf));
-
-        fprintf(stderr, "aaaaaaaaaaaaaaaaaaa\n");
-        fprintf(stderr, "%s\n", buf);
-        fprintf(stderr, "bbbbbbbbbbbbbbbbbbb\n");
-
     }
 
     code_sortie = 0;
     Rio_writen(connfd, &code_sortie, sizeof(int));
-
     Free(buf);
 
     // Recupere status fichier
@@ -66,10 +48,6 @@ void ftp(int connfd) {
     // envoie le contenu du fichier
     Rio_writen(connfd, contenu, buf_off);
     Free(contenu);
-
-//     while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-//         printf("server received %u bytes\n", (unsigned int)n);
-//         Rio_writen(connfd, buf, n);
-//     }
+    
 }
 
